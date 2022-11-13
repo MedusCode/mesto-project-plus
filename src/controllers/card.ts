3fb9ce7fffdd2a86253ce7e03091ebf12a97029a
MedusCode <87340589+MedusCode@users.noreply.github.com>
2022-11-13 05:14:34 +0300

    add: errors for requests

--- src/controllers/card.ts
+++ src/controllers/card.ts
@@ -5,60 +5,90 @@ import { IUser } from '../models/user';
 const getCards = (req: Request, res: Response) => Card.find({})
   .populate<IUser>('owner')
   .then((cards) => res.send({ data: cards }))
-  .catch(() => res.status(500).send({ message: 'Произошла ошибка сервера' })); // TODO: центрелизовать ошибку
+  .catch(() => res.status(500).send({ message: 'Произошла ошибка сервера' }));
 
 const createCard = (req: Request, res: Response) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.status(201).send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message || 'Произошла ошибка сервера' })); // TODO: изменить код ошибки
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка сервера' });
+    });
 };
 
 const deleteCard = (req: Request, res: Response) => {
   const { id } = req.params;
-  Card.findById(id)
+  return Card.findByIdAndRemove(id)
     .then((card) => {
       if (!card) {
-        throw new Error('Фото не найдено');
+        const err = new Error('Карточка не найдена');
+        err.name = 'Not Found';
+        throw err;
       }
-
-      return Card.findByIdAndRemove(id);
+      res.send({ message: 'Карточка успешно удалено' });
     })
-    .then(() => res.send({ message: 'Фото успешно удалено' }))
-    .catch((err) => res.status(500).send({ message: err.message || 'Произошла ошибка сервера' })); // TODO: статус ошибки
+    .catch((err) => {
+      if (err.name === 'Not Found' || err.name === 'CastError') {
+        res.status(404).send({ message: 'Карточка не найдена' });
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка сервера' });
+    });
 };
 
 const likeCard = (req: Request, res: Response) => {
   const { cardId } = req.params;
 
-  return Card.findById(cardId)
+  return Card.findByIdAndUpdate(cardId, { $addToSet: { likes: req.user._id } }, {
+    new: true,
+    runValidators: true,
+  })
+    .populate<IUser>('owner')
     .then((card) => {
       if (!card) {
-        throw new Error('Фото не найдено');
+        const err = new Error('Карточка не найдена');
+        err.name = 'Not Found';
+        throw err;
       }
-
-      return Card.findByIdAndUpdate(cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-        .populate<IUser>('owner');
+      res.send({ data: card });
     })
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message || 'Произошла ошибка сервера' }));
+    .catch((err) => {
+      if (err.name === 'Not Found' || err.name === 'CastError') {
+        res.status(404).send({ message: 'Карточка не найдена' });
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка сервера' });
+    });
 };
 
 const dislikeCard = (req: Request, res: Response) => {
   const { cardId } = req.params;
 
-  return Card.findById(cardId)
+  return Card.findByIdAndUpdate(cardId, { $pull: { likes: req.user._id } }, {
+    new: true,
+    runValidators: true,
+  })
+    .populate<IUser>('owner')
     .then((card) => {
       if (!card) {
-        throw new Error('Фото не найдено');
+        const err = new Error('Карточка не найдена');
+        err.name = 'Not Found';
+        throw err;
       }
-
-      return Card.findByIdAndUpdate(cardId, { $pull: { likes: req.user._id } }, { new: true })
-        .populate<IUser>('owner');
+      res.send({ data: card });
     })
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message || 'Произошла ошибка сервера' }));
+    .catch((err) => {
+      if (err.name === 'Not Found' || err.name === 'CastError') {
+        res.status(404).send({ message: 'Карточка не найдена' });
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка сервера' });
+    });
 };
 
 export {
